6ccd2255dbed1c3dd3fbac45137d9d25aca9e178
Jeremylaby <barycki@student.agh.edu.pl>
2026-04-01 00:24:18 +0200

    clanker working

--- tgk-game/scripts/clanker.gd
+++ tgk-game/scripts/clanker.gd
@@ -23,7 +23,8 @@ var active_input: InputSource = null
 var starting_position: Vector2
 var record_input: bool = true
 var owner_player: Player = null
-
+var previous_position: Vector2 = Vector2.ZERO
+var is_playback: bool = false
 
 func init(pos: Vector2, player: Player) -> void:
 	starting_position = pos
@@ -35,19 +36,42 @@ func _ready() -> void:
 	input_playback.playback_finished.connect(_on_playback_finished)
 
 func _on_playback_finished() -> void:
+	owner_player.set_collision_mask_value(3, false)
 	input_playback.reset()
 	_reset_to_start()
+	owner_player.move_and_slide()
+	owner_player.set_collision_mask_value(3, true)
 
 func _reset_to_start() -> void:
 	global_position = starting_position
+	previous_position = starting_position
 	velocity = Vector2.ZERO
 	current_state = State.IDLE
 	animated_sprite.play("idle")
 
 func _physics_process(delta: float) -> void:
+	previous_position = global_position
 	handle_input()
 	update_state()
 	handle_state(delta)
+	_push_player(delta)
+
+func _push_player(delta: float) -> void:
+	if not owner_player or not is_instance_valid(owner_player):
+		return
+	var delta_pos = global_position - previous_position
+	if delta_pos == Vector2.ZERO:
+		return
+	var distance = owner_player.global_position - global_position
+	if abs(delta_pos.x) > 10 or abs(delta_pos.y) > 10:
+		return
+	if not get_collision_mask_value(2):
+		if distance.y > 0 or abs(distance.y) > 20 or abs(distance.x) > 15:
+			_enable_player_collision()
+	if abs(distance.x) < 9 and abs(distance.y) < 11 and distance.y < 0:
+		owner_player.global_position.y += delta_pos.y * delta
+	elif abs(distance.x) < 9 and abs(distance.y) < 3 and sign(distance.x) == sign(delta_pos.x):
+		owner_player.global_position.x += delta_pos.x
 
 func handle_input() -> void:
 	active_input.update()
@@ -114,6 +138,7 @@ func handle_state(delta: float) -> void:
 			if wants_jump and can_jump:
 				jump_buffer_timer.stop()
 				coyote_timer.stop()
+				_disable_player_collision()
 				movement_component.jump()
 			movement_component.move_horizontal(delta, axis)
 			movement_component.apply_gravity(delta)
@@ -122,9 +147,14 @@ func handle_state(delta: float) -> void:
 			movement_component.apply_gravity(delta)
 
 	movement_component.move_and_slide()
-
-
-
+func _disable_player_collision() -> void:
+	if not owner_player or not is_instance_valid(owner_player):
+		return
+	var distance = owner_player.global_position - global_position
+	if abs(distance.x) < 9 and abs(distance.y) < 11 and distance.y < 0:
+		set_collision_mask_value(2, false)
+func _enable_player_collision() -> void:
+	set_collision_mask_value(2, true)
 
 func disable_control() -> void:
 	record_input = false
@@ -134,3 +164,4 @@ func disable_control() -> void:
 	input_playback.load_recording(input_recorder.get_recording())
 	_reset_to_start()
 	active_input = input_playback
+	is_playback = true
--- tgk-game/scripts/movement_component.gd
+++ tgk-game/scripts/movement_component.gd
@@ -8,6 +8,8 @@ extends Node
 @export var jump_speed := -160.0
 @export var up_gravity := 500
 @export var down_gravity := 1000
+@export var weight := 1.0
+
 var direction: int = 1
 
 func move_horizontal(delta: float, axis: float) -> void:
@@ -15,7 +17,7 @@ func move_horizontal(delta: float, axis: float) -> void:
 		body.velocity.x = move_toward(body.velocity.x, 0, acceleration)
 	else:
 		body.velocity.x = move_toward(body.velocity.x, axis * speed, acceleration)
-	
+
 	if sign(axis) > 0:
 		model.scale.x = 1
 		direction = 1
@@ -23,21 +25,17 @@ func move_horizontal(delta: float, axis: float) -> void:
 		model.scale.x = -1
 		direction = -1
 
-
 func apply_gravity(delta: float) -> void:
 	if body.is_on_floor():
 		return
-	
+
 	if body.velocity.y > 0:
 		body.velocity.y += down_gravity * delta
 	else:
 		body.velocity.y += up_gravity * delta
-	
-
 
 func jump() -> void:
 	body.velocity.y = jump_speed
-
 
 func move_and_slide() -> void:
 	body.move_and_slide()
