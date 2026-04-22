0eea983535321eb9e307a22d48d4ea25927a2d60
Jeremy <stanislaw.barycki@gmail.com>
2026-04-22 17:09:58 +0200

    camera movement adjusted custom smoothing

--- tgk-game/scripts/camera_2d.gd
+++ tgk-game/scripts/camera_2d.gd
@@ -1,10 +1,18 @@
 extends Camera2D
 
-@onready var player: Player = get_parent()
+@export_category("Follow Player")
+@export var player: Player
+@export_category("Camera Smoothing")
+@export var smoothing_enable: bool
+@export_range(1, 10) var smoothing_distance: int = 8
 
-func _physics_process(_delta: float) -> void:
-	global_position = player.get_camera_target()
 
+func _physics_process(delta: float) -> void:
+	var target: Vector2 = player.get_camera_target()
+	if smoothing_enable:
+		target = global_position.lerp(target, (11 - smoothing_distance) * delta)
+		target =(target * zoom).floor() / zoom
+	global_position = target
 
 func setup_camera_limits(tilemap: TileMapLayer) -> void:
 	if not tilemap:
