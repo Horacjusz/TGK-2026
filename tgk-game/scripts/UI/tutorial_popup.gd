6b93fc3acac7aca7d6e48f55f9a645590d4e2a18
Eryk Olejarz <olejarzeryk@gmail.com>
2026-05-27 20:26:12 +0200

    Add typewriter effect to tutorial popup

--- tgk-game/scripts/UI/tutorial_popup.gd
+++ tgk-game/scripts/UI/tutorial_popup.gd
@@ -3,13 +3,29 @@ class_name TutorialPopup
 
 
 var current_tutorial: TutorialData
+var text_tween: Tween
 
 @onready var tutorial_content: RichTextLabel = %TutorialContent
 
 
 func set_tutorial(tutorial: TutorialData) -> void:
 	current_tutorial = tutorial
+	
+	if text_tween and text_tween.is_running():
+		text_tween.kill()
+		
 	tutorial_content.text = tutorial.content
+	tutorial_content.visible_ratio = 0.0
+	
+	text_tween = create_tween()
+	text_tween.tween_property(
+		tutorial_content, 
+		"visible_ratio", 
+		1.0, 
+		1.0
+	)\
+	.set_trans(Tween.TRANS_LINEAR)\
+	.set_ease(Tween.EASE_IN_OUT)
 
 
 func open() -> void:
@@ -31,6 +47,11 @@ func _input(event: InputEvent) -> void:
 
 
 func _handle_interact() -> void:
+	if text_tween and text_tween.is_running():
+		text_tween.kill()
+		tutorial_content.visible_ratio = 1.0
+		return
+	
 	if current_tutorial and current_tutorial.next_tutorial:
 		set_tutorial(current_tutorial.next_tutorial)
 	else:
